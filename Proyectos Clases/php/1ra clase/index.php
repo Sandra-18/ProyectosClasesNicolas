fc1e03c4749fdb8eda942aa1267f79d784020832
Sandra-18 <sanlorenita-18@hotmail.com>
2022-08-09 20:30:34 -0500

    commit
    carusel editado para carlos

---
+++ Proyectos Clases/php/1ra clase/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$variable1 = 'hola mi primer mensaje desde php';
+$variable2 = 'porque somos la trampa';
+
+echo $variable.''.$variable2.'<br>';
+$variable3 = 2;
+$variable4 = 5;
+
+echo $variable3+$variable4;
+
+?>
